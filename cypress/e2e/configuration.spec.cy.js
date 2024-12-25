fa3e1e168a3d24942bd250a9cbbe051cc027d1b7
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-24 20:41:26 -0500

    feat: adds stub configruation test spec, full controls test spec
    Native controls functionality are fully covered in tests.

---
+++ cypress/e2e/configuration.spec.cy.js
@@ -0,0 +1,30 @@
+describe("Slideshow plays automatically", () => {
+  it("Play/Pause button has data-bhc-playing attr set to 'true'", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]").should(
+      "have.attr",
+      "data-bhc-playing",
+      "true",
+    );
+  });
+
+  it("first slide's aria-hidden attr changes to 'true'", () => {
+    cy.visit("/");
+    cy.get("[aria-roledescription='slide']:first-child").should(
+      "have.attr",
+      "aria-hidden",
+      "true",
+    );
+  });
+
+  it("Previous button is disabled", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-previous]").should("have.attr", "disabled");
+  });
+
+  it("Next button is disabled", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-previous]").should("have.attr", "disabled");
+  });
+});
+
--- cypress/e2e/controls.spec.cy.js
+++ cypress/e2e/controls.spec.cy.js
@@ -1,10 +1,158 @@
-describe("Something or other", () => {
-  it("does a thing it should", () => {
-    cy.visit("http://localhost:3000");
-    cy.get('[aria-roledescription="carousel"]')
-      .eq(0)
-      .within(() => {
-        cy.get('[data-bhc-play-pause]').should("have.attr", "data-bhc-playing");
+describe("Slideshow pauses when Play/Pause button clicked", () => {
+  it("Play/Pause button's data-bhc-playing attr changes to 'false'", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .should(
+        "have.attr",
+        "data-bhc-playing",
+        "false",
+      );
+  });
+
+  it("first slide's aria-hidden attr does not change to 'true'", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .then(() => {
+        cy.get("[aria-roledescription='slide']:first-child").should(
+          "have.attr",
+          "aria-hidden",
+          "false",
+        );
+      });
+  });
+
+  it("Previous button is not disabled", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .then(() => {
+        cy.get("[data-bhc-previous]").should("not.have.attr", "disabled");
+      });
+  });
+
+  it("Next button is not disabled", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .then(() => {
+        cy.get("[data-bhc-previous]").should("not.have.attr", "disabled");
+      });
+  });
+});
+
+describe("Slideshow resumes when Play/Pause button clicked", () => {
+  it("slideshow's data-bhc-playing attr changes to true", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .click()
+      .then(() => {
+        cy.get("[data-bhc-play-pause]").should(
+          "have.attr",
+          "data-bhc-playing",
+          "true",
+        );
+      });
+  });
+
+  it("first slide's aria-hidden attr changes to 'true'", () => {
+    cy.visit("/");
+    cy.get("[aria-roledescription='slide']:first-child").should(
+      "have.attr",
+      "aria-hidden",
+      "true",
+    );
+  });
+
+  it("Previous button is disabled", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-previous]").should("have.attr", "disabled");
+  });
+
+  it("Next button is disabled", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-previous]").should("have.attr", "disabled");
+  });
+});
+
+describe("Slideshow goes forward when Next button clicked", () => {
+  it("Slides 1 & 2 change aria-hidden attr to 'true', 'false'", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .then(() => {
+        cy.get("[data-bhc-next]")
+          .click()
+          .then(() => {
+            cy.get("[aria-roledescription='slide']:first-child")
+              .should("have.attr", "aria-hidden", "true");
+            cy.get("[aria-roledescription='slide']:nth-child(2)")
+              .should("have.attr", "aria-hidden", "false");
+          }); 
+      });
+  });
+});
+
+describe("Slideshow goes back when Previous button clicked", () => {
+  it("Slides 1 & 2 change aria-hidden attr to 'false', 'true'", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .then(() => {
+        cy.get("[data-bhc-next]")
+          .click()
+          .then(() => {
+            cy.get("[data-bhc-previous]")
+              .click()
+              .then(() => {
+                cy.get("[aria-roledescription='slide']:first-child")
+                  .should("have.attr", "aria-hidden", "false");
+                cy.get("[aria-roledescription='slide']:nth-child(2)")
+                  .should("have.attr", "aria-hidden", "true");
+              });
+          });
+      });
+  });
+});
+
+describe("Slideshow goes to last slide from first on 'Previous' click", () => {
+  it("Slides 1 & 5 change aria-hidden attr to 'true', 'false'", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .then(() => {
+        cy.get("[data-bhc-previous]")
+          .click()
+          .then(() => {
+            cy.get("[aria-roledescription='slide']:first-child")
+              .should("have.attr", "aria-hidden", "true");
+            cy.get("[aria-roledescription='slide']:last-child")
+              .should("have.attr", "aria-hidden", "false");
+          });
+      });
+  });
+});
+
+describe("Slideshow goes to first slide from last on 'Next' click", () => {
+  it("Slides 1 & 5 change aria-hidden attr to 'false', 'true'", () => {
+    cy.visit("/");
+    cy.get("[data-bhc-play-pause]")
+      .click()
+      .then(() => {
+        cy.get("[data-bhc-previous]")
+          .click()
+          .then(() => {
+            cy.get("[data-bhc-next]")
+              .click()
+              .then(() => {
+                cy.get("[aria-roledescription='slide']:first-child")
+                  .should("have.attr", "aria-hidden", "false");
+                cy.get("[aria-roledescription='slide']:last-child")
+                  .should("have.attr", "aria-hidden", "true");
+              });
+          });
       });
   });
 });
