720cb8b82c90a7bbc4e1c12c31829ed346cc2b43
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-26 16:22:49 -0500

    feat: add js to fixtures for interpreting query-string carousel config

---
+++ cypress/fixtures/js/query2config.js
@@ -0,0 +1,47 @@
+/**
+ * Simple-minded object of config names and validation functions.
+ *
+ * For use in tests only.
+ */
+const configOptions = {
+  autoEnable: (value) => ["true", "false"].includes(value),
+  automatic: (value) => ["true", "false"].includes(value),
+  // Not yet implemented.
+  // controlType: (value) => ["buttons", "tabs"].includes(value),
+  interval: (value) => !isNaN(value),
+  startingIndex: (value) => !isNaN(value),
+};
+
+/**
+ * Gets a simple key-value of configuration options from URL search parameters.
+ */
+const getConfigFromQuery = () => {
+  const configuration = {};
+
+  if (window.location.search) {
+    const paramNames = Object.keys(configOptions);
+    const searchParams = new URLSearchParams(window.location.search);
+    let paramValue;
+
+    for (const [key, value] of searchParams.entries()) {
+      if (!paramNames.includes(key) || !configOptions[key](value)) {
+        continue;
+      }
+
+      switch (key) {
+        case "autoEnable":
+        case "automatic":
+          paramValue = (value === "true");
+          break;
+
+        case "interval":
+        case "startingIndex":
+          paramValue = parseInt(value);
+          break;
+      }
+      configuration[key] = paramValue;
+    }
+  }
+
+  return configuration;
+};
